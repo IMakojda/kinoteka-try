6254c80fc7870ec29d6f9d99c03232700e2e7dc5
Igor Makojda <imakojda@gmail.com>
2022-01-09 18:42:03 +0200

    fetch add

---
+++ src/api/fetchApiService.js
@@ -0,0 +1,51 @@
+import Notiflix from 'notiflix';
+export default class MoviesApiService{
+    constructor(){
+        this.searchQuery="";
+        this.page=1;
+    }
+
+     fetchMovie() {
+        const urlHomePage = 'https://api.themoviedb.org/3/trending/all/day?api_key=3284913a940180ec63ebc0044db949d5';
+        try {
+        
+            return fetch('https://api.themoviedb.org/3/trending/all/day?api_key=3284913a940180ec63ebc0044db949d5')
+                .then(res => res.json())
+                .then(res => console.log(res.results));
+            
+        // .then(console.log);
+        //     const movies = response.json();
+        //     const res = movies.results;
+        // console.log(res);
+        
+        // return res;
+        
+        }
+        
+        catch (error) {
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+           
+        
+        }
+    }
+
+    get popularMovie() {
+        
+    }
+
+    get queryMocie(){
+        return this.searchQuery;
+    }
+
+    set query(newQuery){
+        this.searchQuery=newQuery
+    }
+
+    pageNext (){
+        this.page += 1;
+    }
+
+    resetPage(){
+        this.page = 1;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-fetch('https://api.themoviedb.org/3/trending/all/day?api_key=3284913a940180ec63ebc0044db949d5')
-    .then(res => res.json())
-    .then(console.log)
+import MoviesApiService from './api/fetchApiService';
+
+const newsApi = new MoviesApiService();
+// fetch('https://api.themoviedb.org/3/trending/all/day?api_key=3284913a940180ec63ebc0044db949d5')
+//     .then(res => res.json())
+//     .then(console.log)
+
+
+// // https://api.themoviedb.org/3/trending/all/day?api_key=3284913a940180ec63ebc0044db949d5
+onSearch ()
+function onSearch (){
+    // e.preventDefault();
+    // clearArticles ();
+   
+    // newsApi.query=e.currentTarget.searchQuery.value;
+    // if(newsApi.query===''){
+    //  return errorShow()
+    // }
+    // newsApi.resetPage();
+    // newsApi.fetchImage().then(({hits,totalHits })=>{
+    //   if(totalHits === 0){return errorShow()}
+    //   appendArticlesMarkup(hits)
+    //   showTotalImage(totalHits)
+    //   lazyScroll(0)
+    //   refs.loadMore.style.display="flex"
+    // });
+    newsApi.fetchMovie()
+        // .then(({ original_title, vote_average, vote_count,poster_path }) =>)
+
+}
+
+// function appendArticlesMarkup(articles){
+
+// refs.gallery.insertAdjacentHTML('beforeend',addCardImage(articles));
+// const lightbox = new SimpleLightbox('.gallery a');
+
+// }
+
+// function clearArticles (){
+//     refs.gallery.innerHTML = '';
+// }
